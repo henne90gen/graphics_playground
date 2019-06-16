95b33ee8decb9642d175cb0936378c3f54632185
Henne <henne90gen@gmail.com>
2019-06-16 12:49:46 +0200

    Added sorting to getFilesInDirectory

--- src/logic/util/FileUtils.cpp
+++ src/logic/util/FileUtils.cpp
@@ -1,12 +1,14 @@
 #include "FileUtils.h"
 
 #include <filesystem>
+#include <algorithm>
 
 void getFilesInDirectory(const std::string &directoryPath, std::vector<std::string> &filePaths) {
     for (const auto &entry : std::filesystem::directory_iterator(directoryPath)) {
         std::string fileName = entry.path().u8string();
         filePaths.push_back(fileName);
     }
+    std::sort(filePaths.begin(), filePaths.end());
 }
 
 
