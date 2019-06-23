6c77d1498adb578c2f2bd4a1e13fbb22df983f3f
Henne <henne90gen@gmail.com>
2019-06-24 00:29:50 +0200

    Fixed model loader test

--- src/test/ModelLoaderTest.cpp
+++ src/test/ModelLoaderTest.cpp
@@ -2,19 +2,19 @@
 #include "model_loading/ModelLoader.h"
 
 TEST_CASE("Not existing file is handled correctly") {
-    auto result = ModelLoader::fromFile("not-existing.obj");
-    REQUIRE(result.vertices.empty());
-    REQUIRE(result.normals.empty());
-    REQUIRE(result.textureCoordinates.empty());
-    REQUIRE(result.indices.empty());
+    std::shared_ptr<ModelLoader::RawModel> model = std::make_shared<ModelLoader::RawModel>();
+    auto result = ModelLoader::fromFile("not-existing.obj", model);
+    REQUIRE(result == 1);
+    REQUIRE(model->meshes.empty());
+    REQUIRE(model->materials.empty());
 }
 
 TEST_CASE("Empty file is handled correctly") {
-    auto result = ModelLoader::fromFile("../../../src/test/empty.obj");
-    REQUIRE(result.vertices.empty());
-    REQUIRE(result.normals.empty());
-    REQUIRE(result.textureCoordinates.empty());
-    REQUIRE(result.indices.empty());
+    std::shared_ptr<ModelLoader::RawModel> model = std::make_shared<ModelLoader::RawModel>();
+    auto result = ModelLoader::fromFile("../../../src/test/empty.obj", model);
+    REQUIRE(result == 1);
+    REQUIRE(model->meshes.empty());
+    REQUIRE(model->materials.empty());
 }
 
 static void
@@ -24,7 +24,10 @@ static void
 assertListEquals(const std::string &message, std::vector<glm::ivec3> &expected, std::vector<glm::ivec3> &actual);
 
 TEST_CASE("Simple cube model is loaded correctly") {
-    auto model = ModelLoader::fromFile("../../../src/test/cube.obj");
+    std::shared_ptr<ModelLoader::RawModel> model = std::make_shared<ModelLoader::RawModel>();
+    auto error = ModelLoader::fromFile("../../../src/test/cube.obj", model);
+    REQUIRE(error == 0);
+    REQUIRE(model->meshes.size() == 1);
     std::vector<glm::vec3> vertices = {
             {1.000000,  -1.000000, 1.000000},
             {-1.000000, -1.000000, -1.000000},
@@ -44,14 +47,26 @@ TEST_CASE("Simple cube model is loaded correctly") {
             {1.000000,  -1.000000, -1.000000},
             {-1.000000, 1.000000,  -1.000000},
             {1.000000,  1.000000,  -1.000000},
+            {1.000000,  -1.000000, 1.000000},
             {-1.000000, -1.000000, 1.000000},
+            {-1.000000, -1.000000, -1.000000},
+            {-1.000000, 1.000000,  -1.000000},
             {-1.000000, 1.000000,  1.000000},
             {1.000000,  1.000000,  1.000000},
+            {1.000000,  1.000000,  -1.000000},
+            {1.000000,  1.000000,  1.000000},
+            {1.000000,  -1.000000, 1.000000},
+            {1.000000,  1.000000,  1.000000},
             {-1.000000, 1.000000,  1.000000},
+            {-1.000000, -1.000000, 1.000000},
+            {-1.000000, -1.000000, 1.000000},
             {-1.000000, 1.000000,  1.000000},
+            {-1.000000, 1.000000,  -1.000000},
+            {1.000000,  -1.000000, -1.000000},
             {-1.000000, -1.000000, -1.000000},
+            {-1.000000, 1.000000,  -1.000000},
     };
-    assertListEquals("Vertices", vertices, model.vertices);
+    assertListEquals("Vertices", vertices, model->meshes[0].vertices);
 
     std::vector<glm::vec3> normals = {
             {0.0000,  -1.0000, 0.0000},
@@ -73,32 +88,46 @@ TEST_CASE("Simple cube model is loaded correctly") {
             {0.0000,  0.0000,  -1.0000},
             {0.0000,  0.0000,  -1.0000},
             {0.0000,  -1.0000, 0.0000},
+            {0.0000,  -1.0000, 0.0000},
+            {0.0000,  -1.0000, 0.0000},
+            {0.0000,  1.0000,  0.0000},
+            {0.0000,  1.0000,  0.0000},
             {0.0000,  1.0000,  0.0000},
             {1.0000,  -0.0000, 0.0000},
+            {1.0000,  -0.0000, 0.0000},
+            {1.0000,  -0.0000, 0.0000},
+            {0.0000,  -0.0000, 1.0000},
+            {0.0000,  -0.0000, 1.0000},
             {0.0000,  -0.0000, 1.0000},
             {-1.0000, -0.0000, -0.0000},
+            {-1.0000, -0.0000, -0.0000},
+            {-1.0000, -0.0000, -0.0000},
+            {0.0000,  0.0000,  -1.0000},
+            {0.0000,  0.0000,  -1.0000},
             {0.0000,  0.0000,  -1.0000},
     };
-    assertListEquals("Normals", normals, model.normals);
+    assertListEquals("Normals", normals, model->meshes[0].normals);
 
     std::vector<glm::ivec3> indices = {
-            {0,  1,  2},
-            {3,  4,  5},
-            {6,  7,  8},
-            {9,  10, 11},
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {9, 10, 11},
             {12, 13, 14},
             {15, 16, 17},
-            {0,  18, 1},
-            {3,  19, 4},
-            {6,  20, 7},
-            {9,  21, 10},
-            {12, 22, 13},
-            {15, 23, 16},
+            {18, 19, 20},
+            {21, 22, 23},
+            {24, 25, 26},
+            {27, 28, 29},
+            {30, 31, 32},
+            {33, 34, 35},
     };
-    assertListEquals("Indices", indices, model.indices);
+    assertListEquals("Indices", indices, model->meshes[0].indices);
 
-    REQUIRE(model.textureCoordinates.empty());
+    REQUIRE(model->meshes[0].textureCoordinates.empty());
 }
+
+#include <iostream>
 
 static void
 assertListEquals(const std::string &message, std::vector<glm::vec3> &expected, std::vector<glm::vec3> &actual) {
