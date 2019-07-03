d2df973695b4a7f3feb088b8023498cc3a700539
Henne <henne90gen@gmail.com>
2019-07-03 18:02:04 +0200

    Fixing linux build with zlib and libpng

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 cd build
-cmake -DRUN_CLANG_TIDY=ON ..
-#cmake -DRUN_CLANG_TIDY=OFF ..
+#cmake -DRUN_CLANG_TIDY=ON ..
+cmake -DRUN_CLANG_TIDY=OFF ..
 time make -j 8 2> build_report.csv
 make test
