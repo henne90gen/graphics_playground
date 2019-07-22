13b75b4eb7306d5351cd5961847d9a9521294d5f
Henne <henne90gen@gmail.com>
2019-07-23 01:00:38 +0200

    Added build type switch to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 cd build
-#cmake -DRUN_CLANG_TIDY=ON ..
-cmake -DRUN_CLANG_TIDY=OFF ..
+#cmake -DCMAKE_BUILD_TYPE=Release -DRUN_CLANG_TIDY=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DRUN_CLANG_TIDY=OFF ..
 time make -j 8 2> build_report.csv
 make test
