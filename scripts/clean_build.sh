f113b74f94b2675b69e8b039b727f6a231d984ca
Henne <henne90gen@gmail.com>
2019-07-23 01:20:07 +0200

    Merged build scripts

--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+rm build -rf
 mkdir -p build
 cd build
+
 #cmake -DCMAKE_BUILD_TYPE=Release -DRUN_CLANG_TIDY=ON ..
 cmake -DCMAKE_BUILD_TYPE=Release -DRUN_CLANG_TIDY=OFF ..
 time make -j 8 2> build_report.csv
+
 make test
