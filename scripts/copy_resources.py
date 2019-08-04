2a2b78af9416afb8f137d7bea7a8fcdf306d8312
Henne <henne90gen@gmail.com>
2019-08-04 22:01:08 +0200

    Added fail safe, in case the requested file or directory doesn't exist

--- scripts/copy_resources.py
+++ scripts/copy_resources.py
@@ -9,6 +9,10 @@ def run_copy_resources(base_dir: str, files: List[str], dest: str):
         src = f"{base_dir}/{f}"
         dst = f"{dest}/{f}"
 
+        if not os.path.exists(src):
+            print(f"Could not find resource '{src}'")
+            continue
+
         destination_directory = "/".join(dst.split("/")[:-1])
         if not os.path.exists(destination_directory):
             os.makedirs(destination_directory)
