d11b5fc160151cfe0279a5967cd5079a2ab9a5aa
Henne <henne90gen@gmail.com>
2019-08-09 12:35:23 +0200

    Refactored timing utility

--- src/app/util/TimeUtils.h
+++ src/app/util/TimeUtils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <time.h>
+
 #define TIME_IT(code) \
 { \
     timespec start = {}, end = {}; \
@@ -7,17 +9,8 @@
     code; \
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); \
     timespec d = diff(start, end); \
-    std::cout << d.tv_sec << ":" << d.tv_nsec << std::endl; \
+    double secs = (double) d.tv_sec + ((double) d.tv_nsec / 1000000000.0); \
+    std::cout << secs << "s " << secs * 1000.0 << "ms" << std::endl; \
 }
 
-timespec diff(timespec start, timespec end) {
-    timespec temp = {};
-    if ((end.tv_nsec - start.tv_nsec) < 0) {
-        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
-        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec - start.tv_sec;
-        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
-    }
-    return temp;
-}
+timespec diff(timespec start, timespec end);
