aac2854092423b51db375015211b089f1edaa2cd
Henne <henne90gen@gmail.com>
2019-10-27 13:35:02 +0100

    Added sphere helper function

--- src/app/util/RenderUtils.cpp
+++ src/app/util/RenderUtils.cpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-std::shared_ptr<VertexArray> createCubeVA(std::shared_ptr<Shader> shader) {
+std::shared_ptr<VertexArray> createCubeVA(const std::shared_ptr<Shader> &shader) {
     std::vector<glm::vec3> vertices = {
           // back
           {-1.0F, -1.0F, -1.0F}, // 0
@@ -41,6 +41,61 @@ std::shared_ptr<VertexArray> createCubeVA(std::shared_ptr<Shader> shader) {
           {4, 5, 1}, // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
           {4, 1, 0}, // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     };
+
+    auto array = std::make_shared<VertexArray>(shader);
+    BufferLayout layout = {
+          {ShaderDataType::Float3, "a_Position"},
+    };
+    std::shared_ptr<VertexBuffer> vb = std::make_shared<VertexBuffer>(vertices, layout);
+    array->addVertexBuffer(vb);
+    std::shared_ptr<IndexBuffer> ib = std::make_shared<IndexBuffer>(indices);
+    array->setIndexBuffer(ib);
+    return array;
+}
+
+std::shared_ptr<VertexArray> createSphereVA(const std::shared_ptr<Shader> &shader, const int sectorCount,
+                                            const int stackCount) {
+    std::vector<glm::vec3> vertices = {};
+    std::vector<glm::ivec3> indices = {};
+
+    float sectorStep = glm::two_pi<float>() / (float)sectorCount;
+    float stackStep = glm::pi<float>() / (float)stackCount;
+    float sectorAngle, stackAngle;
+    float x, y, z, xy;
+
+    for (int i = 0; i <= stackCount; i++) {
+        stackAngle = glm::half_pi<float>() - (float)i * stackStep; // starting from pi/2 to -pi/2
+        xy = glm::cos(stackAngle);
+        z = glm::sin(stackAngle);
+
+        // add (sectorCount+1) vertices per stack
+        // the first and last vertices have same position and normal, but different tex coords
+        for (int j = 0; j <= sectorCount; j++) {
+            sectorAngle = (float)j * sectorStep;
+            x = xy * glm::cos(sectorAngle);
+            y = xy * glm::sin(sectorAngle);
+            vertices.emplace_back(x, y, z);
+        }
+    }
+
+    int k1, k2;
+    for (int i = 0; i < stackCount; ++i) {
+        k1 = i * (sectorCount + 1); // beginning of current stack
+        k2 = k1 + sectorCount + 1;  // beginning of next stack
+
+        for (int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
+            // 2 triangles per sector excluding first and last stacks
+            // k1 => k2 => k1+1
+            if (i != 0) {
+                indices.emplace_back(k1, k2, k1 + 1);
+            }
+
+            // k1+1 => k2 => k2+1
+            if (i != (stackCount - 1)) {
+                indices.emplace_back(k1 + 1, k2, k2 + 1);
+            }
+        }
+    }
 
     auto array = std::make_shared<VertexArray>(shader);
     BufferLayout layout = {
--- src/app/util/RenderUtils.h
+++ src/app/util/RenderUtils.h
@@ -1,8 +1,18 @@
 #pragma once
 
-#include <memory>
 #include <glm/glm.hpp>
+#include <memory>
 
 #include "opengl/VertexArray.h"
 
-std::shared_ptr<VertexArray> createCubeVA(std::shared_ptr<Shader> shader);
+std::shared_ptr<VertexArray> createCubeVA(const std::shared_ptr<Shader> &shader);
+
+/**
+ * Copied from http://www.songho.ca/opengl/gl_sphere.html
+ * @param shader
+ * @param sectorCount "rings" around the sphere
+ * @param stackCount "strips" from top to bottom of the sphere
+ * @return
+ */
+std::shared_ptr<VertexArray> createSphereVA(const std::shared_ptr<Shader> &shader, int sectorCount = 36,
+                                            int stackCount = 18);
