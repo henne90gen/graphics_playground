84e3e0612984b38b3ccd9a8988c35f1a91db1b22
Henne <henne90gen@gmail.com>
2019-10-28 20:20:55 +0100

    Refactored recording types in back-menu

--- src/app/scenes/Scene.cpp
+++ src/app/scenes/Scene.cpp
@@ -3,7 +3,7 @@
 void Scene::renderBackMenu() {
     ImGui::Begin(name.c_str());
     ImGui::SetWindowPos(ImVec2(0, 0));
-    const unsigned int windowWidth = 330;
+    const unsigned int windowWidth = 150;
     const unsigned int windowHeight = 130;
     ImGui::SetWindowSize(ImVec2(windowWidth, windowHeight));
 
@@ -11,9 +11,11 @@ void Scene::renderBackMenu() {
         data.recorder.takeScreenshot();
     }
 
-    static const std::array<const char *, 2> items = {"GIF", "MP4"};
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    ImGui::Combo("Recording Type", reinterpret_cast<int *>(&data.recorder.recordingType), items.data(), items.size());
+    ImGui::RadioButton("GIF", reinterpret_cast<int *>(&data.recorder.recordingType),
+                       ScreenRecorder::RecordingType::GIF);
+    ImGui::SameLine();
+    ImGui::RadioButton("MP4", reinterpret_cast<int *>(&data.recorder.recordingType),
+                       ScreenRecorder::RecordingType::MP4);
 
     if (data.recorder.isRecording()) {
         if (ImGui::Button("Stop Recording")) {
