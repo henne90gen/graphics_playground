995d48b23bc77b071cd168edc3c5c68ce60079de
Henne <henne90gen@gmail.com>
2019-12-14 08:35:08 +0100

    Fixed some minor transformation bugs

--- src/app/scenes/cube/Cube.cpp
+++ src/app/scenes/cube/Cube.cpp
@@ -71,36 +71,39 @@ void Cube::setup() {
 void Cube::destroy() {}
 
 void Cube::tick() {
-    static auto translation = glm::vec3(0.0F, 0.0F, -4.5F); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+    static auto translation = glm::vec3(0.0F, 1.0F, -4.5F); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
     static auto modelRotation = glm::vec3(0.0F);
     static auto cameraRotation = glm::vec3(0.0F);
-    static float scale = 1.0F;
+    static float scale = 0.5F;
 
     ImGui::Begin("Settings");
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-type-reinterpret-cast)
-    ImGui::DragFloat3("Position", reinterpret_cast<float *>(&translation), 0.05F);
+    ImGui::DragFloat3("Model Position", reinterpret_cast<float *>(&translation), 0.05F);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-type-reinterpret-cast)
-    ImGui::DragFloat3("Rotation", reinterpret_cast<float *>(&modelRotation), 0.01F);
+    ImGui::DragFloat3("Model Rotation", reinterpret_cast<float *>(&modelRotation), 0.01F);
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    ImGui::DragFloat("Model Scale", &scale, 0.001F);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-type-reinterpret-cast)
     ImGui::DragFloat3("Camera Rotation", reinterpret_cast<float *>(&cameraRotation), 0.01F);
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    ImGui::DragFloat("Scale", &scale, 0.001F);
     ImGui::End();
 
     shader->bind();
     vertexArray->bind();
 
     glm::mat4 modelMatrix = glm::mat4(1.0F);
-    modelMatrix = glm::scale(modelMatrix, glm::vec3(scale));
+    modelMatrix = glm::translate(modelMatrix, translation);
     modelMatrix = glm::rotate(modelMatrix, modelRotation.x, glm::vec3(1, 0, 0));
     modelMatrix = glm::rotate(modelMatrix, modelRotation.y, glm::vec3(0, 1, 0));
     modelMatrix = glm::rotate(modelMatrix, modelRotation.z, glm::vec3(0, 0, 1));
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(scale, 1.0,1.0));
+
     glm::mat4 viewMatrix = glm::mat4(1.0F);
     viewMatrix = glm::rotate(viewMatrix, cameraRotation.x, glm::vec3(1, 0, 0));
     viewMatrix = glm::rotate(viewMatrix, cameraRotation.y, glm::vec3(0, 1, 0));
     viewMatrix = glm::rotate(viewMatrix, cameraRotation.z, glm::vec3(0, 0, 1));
-    viewMatrix = glm::translate(viewMatrix, translation);
+
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(FIELD_OF_VIEW), getAspectRatio(), Z_NEAR, Z_FAR);
+
     shader->setUniform("modelMatrix", modelMatrix);
     shader->setUniform("viewMatrix", viewMatrix);
     shader->setUniform("projectionMatrix", projectionMatrix);
