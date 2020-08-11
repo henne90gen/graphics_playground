ea035f5998376230ba535fbc8ac6a0d53651c8ac
Hendrik Müller <henne90gen@gmail.com>
2020-08-12 00:10:28 +0200

    Finished shp file loading

--- src/logic/gis/ShpLoaderTest.cpp
+++ src/logic/gis/ShpLoaderTest.cpp
@@ -2,7 +2,20 @@
 
 #include "ShpLoader.h"
 
-int main() { 
-    std::cout << "Hello World!" << std::endl; 
-    loadShpFile("../../../gis_data/gravimetry/gravi.shp");
+int main() {
+    ShpData shpData;
+    bool success = loadShpFile("../../../gis_data/gravimetry/gravi.shp", shpData);
+    if (!success) {
+        std::cout << "Could not load shp file" << std::endl;
+    } else {
+        std::cout << "Successfully loaded shp file" << std::endl;
+    }
+
+    DbfData dbfData;
+    success = loadDbfFile("../../../gis_data/gravimetry/gravi.dbf", dbfData);
+    if (!success) {
+        std::cout << "Could not load dbf file" << std::endl;
+    } else {
+        std::cout << "Successfully loaded dbf file" << std::endl;
+    }
 }
