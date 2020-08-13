0ce17db60c9d1b73b45e8a6c30130ad136d92ad5
Henne <henne90gen@gmail.com>
2020-08-13 19:54:11 +0200

    Added test data and updated test

--- src/test/gis/XyzLoaderTest.cpp
+++ src/test/gis/XyzLoaderTest.cpp
@@ -6,7 +6,7 @@
 
 TEST_CASE("Can load xyz directory") {
     std::vector<glm::vec3> result = {};
-    bool success = loadXyzDir("../../../gis_data/dtm", result);
+    bool success = loadXyzDir("../../../src/test/gis/dtm", result);
     REQUIRE(success);
-    REQUIRE(result.size() == 46729001);
+    REQUIRE(result.size() == 38220);
 }
