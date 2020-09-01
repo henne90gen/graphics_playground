dee867bfeec0260bdef45314d7b32fc856496381
Hendrik Müller <henne90gen@gmail.com>
2020-09-01 23:56:57 +0200

    Implemented QuadTree and added benchmark for QuadTree

--- src/logic/quad_tree/QuadTree.h
+++ src/logic/quad_tree/QuadTree.h
@@ -5,56 +5,64 @@
 
 #include "util/BoundingBox.h"
 
-template <typename T> struct Node {
+/*
+ * Running the benchmark revealed that K=256 is a pretty optimal size for the tree.
+ */
+
+inline float distanceSq(const glm::vec3 &p1, const glm::vec3 &p2) {
+    auto vec = p1 - p2;
+    return vec.x * vec.x + vec.y * vec.y + vec.z * vec.z;
+}
+
+template <typename T, unsigned int K> struct QuadTree {
     using Element = typename std::pair<glm::vec3, T>;
     using Iterator = typename std::vector<Element>::iterator;
 
-    Iterator begin;
-    Iterator end;
-    BoundingBox3 bb;
-    bool parity = false;
-    Node<T> *left = nullptr;
-    Node<T> *right = nullptr;
+    struct Node {
+        Iterator begin;
+        Iterator end;
+        BoundingBox3 bb = {};
+        bool parity = false;
+        Node *left = nullptr;
+        Node *right = nullptr;
 
-    Node(Iterator begin, Iterator end) : begin(begin), end(end) { init(); }
-    Node(Iterator begin, Iterator end, bool parity) : begin(begin), end(end), parity(parity) { init(); }
+        Node(Iterator begin, Iterator end) : begin(begin), end(end) { init(); }
+        Node(Iterator begin, Iterator end, bool parity) : begin(begin), end(end), parity(parity) { init(); }
 
-    void init() {
-        std::function<bool(const Element &, const Element &)> comp;
-        if (parity) {
-            comp = [](const Element &l, const Element &r) { return l.first.x < r.first.x; };
-        } else {
-            comp = [](const Element &l, const Element &r) { return l.first.z < r.first.z; };
-        }
-        std::sort(begin, end, comp);
-
-        long numElements = end - begin;
-        if (numElements <= 5) {
-            for (Iterator itr = begin; itr != end; itr++) {
-                bb.update(itr->first);
+        void init() {
+            std::function<bool(const Element &, const Element &)> comp;
+            if (parity) {
+                comp = [](const Element &l, const Element &r) { return l.first.x < r.first.x; };
+            } else {
+                comp = [](const Element &l, const Element &r) { return l.first.z < r.first.z; };
             }
-        } else {
-            long numElementsHalf = numElements / 2;
-            left = new Node<T>(begin, begin + numElementsHalf, !parity);
-            bb.update(left->bb);
+            std::sort(begin, end, comp);
 
-            right = new Node<T>(begin + numElementsHalf, end, !parity);
-            bb.update(right->bb);
+            long numElements = end - begin;
+            if (numElements <= K) {
+                for (Iterator itr = begin; itr != end; itr++) {
+                    bb.update(itr->first);
+                }
+            } else {
+                long numElementsHalf = numElements / 2;
+                left = new Node(begin, begin + numElementsHalf, !parity);
+                bb.update(left->bb);
+
+                right = new Node(begin + numElementsHalf, end, !parity);
+                bb.update(right->bb);
+            }
         }
-    }
-};
 
-template <typename T> struct QuadTree {
-    using Element = typename std::pair<glm::vec3, T>;
-    using Iterator = typename std::vector<Element>::iterator;
+        inline bool isLeaf() { return left == nullptr; }
+    };
 
     std::vector<Element> elements = {};
-    Node<T> *root = new Node<T>(elements.begin(), elements.end());
+    Node *root = new Node(elements.begin(), elements.end());
 
     void insert(const glm::vec3 &point, T data) {
         delete root;
         elements.push_back(std::make_pair(point, data));
-        root = new Node<T>(elements.begin(), elements.end());
+        root = new Node(elements.begin(), elements.end());
     }
 
     void insert(const std::vector<Element> &newElements) {
@@ -63,23 +71,42 @@ template <typename T> struct QuadTree {
         for (const auto &element : newElements) {
             elements.push_back(element);
         }
-        root = new Node<T>(elements.begin(), elements.end());
+        root = new Node(elements.begin(), elements.end());
     }
 
     T get(const glm::vec3 &query) {
-        std::vector<Node<T> *> nodeQueue = {root};
+        std::vector<Node *> nodeQueue = {root};
         float minDistSq = std::numeric_limits<float>::max();
         T closestElement;
         while (!nodeQueue.empty()) {
-            Node<T> *currentNode = nodeQueue.back();
+            Node *currentNode = nodeQueue.back();
             nodeQueue.pop_back();
 
-            for (Iterator itr = currentNode->begin; itr != currentNode->end; itr++) {
-                auto distVec = itr->first - query;
-                float distSq = distVec.x * distVec.x + distVec.y * distVec.y + distVec.z * distVec.z;
-                if (distSq < minDistSq) {
-                    minDistSq = distSq;
-                    closestElement = itr->second;
+            auto closestPoint = currentNode->bb.closestPointOnSurface(query);
+            auto distSq = distanceSq(closestPoint, query);
+            if (distSq >= minDistSq) {
+                continue;
+            }
+
+            if (!currentNode->isLeaf()) {
+                glm::vec3 closestPointLeft = currentNode->left->bb.closestPointOnSurface(query);
+                glm::vec3 closestPointRight = currentNode->right->bb.closestPointOnSurface(query);
+                float distLeftSq = distanceSq(closestPointLeft, query);
+                float distRightSq = distanceSq(closestPointRight, query);
+                if (distLeftSq < distRightSq) {
+                    nodeQueue.push_back(currentNode->right);
+                    nodeQueue.push_back(currentNode->left);
+                } else {
+                    nodeQueue.push_back(currentNode->left);
+                    nodeQueue.push_back(currentNode->right);
+                }
+            } else {
+                for (Iterator itr = currentNode->begin; itr != currentNode->end; itr++) {
+                    float distSq = distanceSq(itr->first, query);
+                    if (distSq < minDistSq) {
+                        minDistSq = distSq;
+                        closestElement = itr->second;
+                    }
                 }
             }
         }
---
+++ src/logic/quad_tree/QuadTreeBench.cpp
@@ -0,0 +1,105 @@
+#include <benchmark/benchmark.h>
+
+#include "QuadTree.h"
+
+constexpr unsigned int start = 16;
+constexpr unsigned int end = 65536 * 8;
+
+template <unsigned int K> QuadTree<unsigned int, K> createTree(const unsigned int size) {
+    QuadTree<unsigned int, K> tree = {};
+    std::vector<std::pair<glm::vec3, unsigned int>> elements = {};
+    const unsigned int dimension = std::sqrt(size);
+    for (unsigned int i = 0; i < size; i++) {
+        unsigned int x = i % dimension;
+        unsigned int z = i / dimension;
+        elements.push_back(std::make_pair(glm::vec3(x, 0, z), x * dimension + z));
+    }
+    tree.insert(elements);
+    return tree;
+}
+
+static void K8(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<8>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K8)->Range(start, end);
+
+static void K16(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<16>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K16)->Range(start, end);
+
+static void K32(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<32>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K32)->Range(start, end);
+
+static void K64(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<64>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K64)->Range(start, end);
+
+static void K128(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<128>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K128)->Range(start, end);
+
+static void K256(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<256>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K256)->Range(start, end);
+
+static void K512(benchmark::State &state) {
+    const unsigned int size = state.range(0);
+    auto tree = createTree<512>(size);
+
+    for (auto _ : state) {
+        glm::vec3 query = {0, 0, 0};
+        auto result = tree.get(query);
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(K512)->Range(start, end);
+
+BENCHMARK_MAIN();
