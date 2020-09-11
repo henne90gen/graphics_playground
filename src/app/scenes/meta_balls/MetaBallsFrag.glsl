31849375a7164607f7f1a62325bb45ffaf65aaf6
Henne <henne90gen@gmail.com>
2020-09-11 09:24:06 +0200

    Fixed MetaBalls shader

--- src/app/scenes/meta_balls/MetaBallsFrag.glsl
+++ src/app/scenes/meta_balls/MetaBallsFrag.glsl
@@ -1,7 +1,12 @@
 #version 330 core
 
+in vec3 v_FinalPosition;
+
+uniform vec3 u_Dimensions;
+
 out vec4 color;
 
 void main() {
-    color = vec4(1.0, 1.0, 1.0, 1.0);
+    vec3 colorv3 = v_FinalPosition / u_Dimensions;
+    color = vec4(colorv3, 1.0);
 }
--- src/app/scenes/meta_balls/MetaBallsVert.glsl
+++ src/app/scenes/meta_balls/MetaBallsVert.glsl
@@ -1,7 +1,15 @@
 #version 330 core
 
-attribute vec3 a_Position;
+in vec3 position;
+
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+uniform vec3 u_Offset;
+
+out vec3 v_FinalPosition;
 
 void main() {
-    gl_Position = vec4(a_Position, 1.0);
+    v_FinalPosition = position + u_Offset;
+    gl_Position = u_Projection * u_View * u_Model * vec4(v_FinalPosition, 1.0);
 }
