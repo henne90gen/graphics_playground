f38a8fc2f5a90e3951c9cb528ca48dc3bb7287b1
Hendrik Müller <henne90gen@gmail.com>
2020-11-28 18:54:06 +0100

    Generalized l-system generation and rendering

--- src/app/scenes/l_systems/LSystems.cpp
+++ src/app/scenes/l_systems/LSystems.cpp
@@ -1,5 +1,7 @@
 #include "LSystems.h"
 
+#include "util/ImGuiUtils.h"
+
 const float FIELD_OF_VIEW = 45.0F;
 const float Z_NEAR = 0.1F;
 const float Z_FAR = 10000.0F;
@@ -29,17 +31,28 @@ void LSystems::setup() {
 void LSystems::destroy() {}
 
 void LSystems::tick() {
+    const std::array<std::function<void(std::vector<glm::vec3> &, unsigned int)>, 2> lSystems = {
+          dragonCurve,  //
+          fractalPlant, //
+    };
+    const std::vector<std::string> lSystemNames = {
+          "Dragon Curve",  //
+          "Fractal Plant", //
+    };
     static auto cameraPosition = glm::vec3(0.0F, 0.0F, -100.0F);
     static auto cameraRotation = glm::vec3();
     static auto numIterations = 3;
-    static auto timerSeconds = 1.0;
     static auto vertexCount = 0;
+    static unsigned int currentLSystem = 0;
+    auto previousNumIterations = numIterations;
+    auto previousLSystem = currentLSystem;
 
     ImGui::Begin("Settings");
     ImGui::DragFloat3("Camera Position", reinterpret_cast<float *>(&cameraPosition), 0.01F);
     ImGui::DragFloat3("Camera Rotation", reinterpret_cast<float *>(&cameraRotation), 0.01F);
     ImGui::Separator();
     ImGui::SliderInt("Number of Iterations", &numIterations, 0, 20);
+    ImGui::ListBox("L System", currentLSystem, lSystemNames);
     ImGui::End();
 
     shader->bind();
@@ -52,16 +65,10 @@ void LSystems::tick() {
     const auto modelMatrix = glm::mat4(1.0);
     shader->setUniform("u_Model", modelMatrix);
 
-    timerSeconds += getLastFrameTime();
-    if (timerSeconds >= 0.5) {
-        timerSeconds = 0.0;
-        numIterations++;
-        if (numIterations > 20) {
-            numIterations = 5;
-        }
+    if (numIterations != previousNumIterations || currentLSystem != previousLSystem || vertexCount == 0) {
         std::vector<glm::vec3> vertices = {};
 
-        dragonCurve(vertices, numIterations);
+        lSystems[currentLSystem](vertices, numIterations);
 
         vb->update(vertices);
         vertexCount = vertices.size();
@@ -73,56 +80,75 @@ void LSystems::tick() {
     shader->unbind();
 }
 
-void line(std::vector<glm::vec3> &vertices, float x1, float y1, float x2, float y2) {
+inline void line(std::vector<glm::vec3> &vertices, float x1, float y1, float x2, float y2) {
     vertices.emplace_back(x1, y1, 0.0);
     vertices.emplace_back(x2, y2, 0.0);
 }
 
-void simulateLSystem(const unsigned int numIterations, const std::string &startSequence,
-                     const std::function<std::string(char)> &sequenceUpdateFunc,
-                     const std::function<void(char)> &turtleFunc) {
-    std::string sequence = startSequence;
+void simulateLSystem(std::vector<glm::vec3> &vertices, const unsigned int numIterations, const LSystem &l) {
+    std::string sequence = l.startSequence;
     for (unsigned int i = 0; i < numIterations; i++) {
         std::string newSequence;
         for (char c : sequence) {
-            newSequence += sequenceUpdateFunc(c);
+            auto result = l.replacements.find(c);
+            if (result != l.replacements.end()) {
+                newSequence += result->second;
+            } else {
+                newSequence += c;
+            }
         }
         sequence = newSequence;
     }
 
+    struct StackElement {
+        StackElement(float x, float y, float angle) : x(x), y(y), angle(angle){};
+        float x;
+        float y;
+        float angle;
+    };
+    std::vector<StackElement> stack = {};
+    float x = 0.0F;
+    float y = 0.0F;
+    float angle = glm::pi<float>() / 2.0F;
     for (char c : sequence) {
-        turtleFunc(c);
+        if (c == 'F') {
+            const float nextX = x + l.d * std::cos(angle);
+            const float nextY = y + l.d * std::sin(angle);
+            line(vertices, x, y, nextX, nextY);
+            x = nextX;
+            y = nextY;
+        } else if (c == '[') {
+            stack.emplace_back(x, y, angle);
+        } else if (c == ']') {
+            auto &elem = stack.back();
+            x = elem.x;
+            y = elem.y;
+            angle = elem.angle;
+            stack.pop_back();
+        } else if (c == '+') {
+            angle -= l.angleDelta;
+        } else if (c == '-') {
+            angle += l.angleDelta;
+        }
     }
 }
 
 void dragonCurve(std::vector<glm::vec3> &vertices, const unsigned int numIterations) {
-    const float d = 0.05F;
-    const float angleDelta = glm::pi<float>() / 2.0F;
-    float x = 0.0F;
-    float y = 0.0F;
-    float angle = 0.0F;
-    simulateLSystem(
-          numIterations, "FX",
-          [](char c) -> std::string {
-              if (c == 'X') {
-                  return "X+YF+";
-              }
-              if (c == 'Y') {
-                  return "-FX-Y";
-              }
-              return std::string(1, c);
-          },
-          [&vertices, &d, &angleDelta, &x, &y, &angle](char c) {
-              if (c == 'F') {
-                  const float nextX = x + d * std::cos(angle);
-                  const float nextY = y + d * std::sin(angle);
-                  line(vertices, x, y, nextX, nextY);
-                  x = nextX;
-                  y = nextY;
-              } else if (c == '-') {
-                  angle -= angleDelta;
-              } else if (c == '+') {
-                  angle += angleDelta;
-              }
-          });
+    const float d = 0.5F;
+    const float angleDelta = glm::pi<float>() / -2.0F;
+    std::unordered_map<char, std::string> replacements = {};
+    replacements.emplace('X', std::string("X+YF+"));
+    replacements.emplace('Y', std::string("-FX-Y"));
+    LSystem l = {"FX", replacements, d, angleDelta};
+    simulateLSystem(vertices, numIterations, l);
+}
+
+void fractalPlant(std::vector<glm::vec3> &vertices, const unsigned int numIterations) {
+    const float d = 0.05;
+    const float angleDelta = glm::pi<float>() / 7.2F;
+    std::unordered_map<char, std::string> replacements = {};
+    replacements.emplace('X', std::string("F+[[X]-X]-F[-FX]+X"));
+    replacements.emplace('F', std::string("FF"));
+    LSystem l = {"X", replacements, d, angleDelta};
+    simulateLSystem(vertices, numIterations, l);
 }
