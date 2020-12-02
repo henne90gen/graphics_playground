29a3ddd094bfeefbcbba0b1941e54f90f1a84c56
henne90gen <henne90gen@gmail.com>
2020-12-02 23:09:21 +0100

    Fixed critical timing bug

--- src/core/Scene.h
+++ src/core/Scene.h
@@ -58,7 +58,7 @@ class Scene {
     const std::string name;
     SceneData data;
     double timeDelta = 0.0;
-    long lastTimeNs = 0L;
+    long long lastTimeNs = 0L;
 
     unsigned int width = 0;
     unsigned int height = 0;
