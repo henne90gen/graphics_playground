f562689c8feb3801ff2bb295a9aae5e77e4e4ba4
Hendrik Müller <henne90gen@gmail.com>
2020-12-30 11:40:02 +0100

    Implemented different lighting functions

--- src/scenes/landscape/LandscapeFrag.glsl
+++ src/scenes/landscape/LandscapeFrag.glsl
@@ -6,6 +6,25 @@ struct NoiseLayer {
     bool enabled;
 };
 
+struct DirLight {
+    vec3 direction;
+    vec3 color;
+    float power;
+};
+
+struct PointLight {
+    vec3 position;
+    vec3 color;
+    float power;
+};
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
 in vec2 uv_frag_in;
 in vec3 normal_frag_in;
 in vec3 tangent_frag_in;
@@ -21,7 +40,6 @@ uniform vec3 cameraPosition;
 uniform bool showNormals;
 uniform bool showTangents;
 uniform bool showUVs;
-uniform bool useNormalMap;
 
 const int MAX_NUM_NOISE_LAYERS = 15;
 uniform NoiseLayer noiseLayers[MAX_NUM_NOISE_LAYERS];
@@ -46,6 +64,7 @@ vec3 getSurfaceColor(float height) {
 
     float noiseMin = 0.0F;
     float noiseMax = 0.0F;
+    // TODO this can be done in the TES instead to increase performance
     for (int i = 0; i < numNoiseLayers; i++) {
         if (!noiseLayers[i].enabled) {
             continue;
@@ -71,17 +90,12 @@ vec3 getSurfaceColor(float height) {
     }
 }
 
-void main() {
-    vec3 normal = normalize(normalMatrix * normal_frag_in);
-    vec3 tangent = normalize(normalMatrix * tangent_frag_in);
-    vec3 bitangent = normalize(normalMatrix * bitangent_frag_in);
-
+vec4 debugColors(vec3 normal, vec3 tangent, vec3 bitangent) {
     if (showNormals) {
         //        normal = normal_frag_in;
         normal += 1.0F;
         normal /= 2.0F;
-        color = vec4(normal, 1.0F);
-        return;
+        return vec4(normal, 1.0F);
     }
 
     if (showTangents) {
@@ -93,23 +107,59 @@ void main() {
         bitangent += 1.0F;
         bitangent /= 2.0F;
 
-        color = vec4(tangent.y, bitangent.y, 0.0F, 1.0F);
-        return;
+        return vec4(tangent.y, bitangent.y, 0.0F, 1.0F);
     }
 
     if (showUVs) {
         // this uv is only used for visualization (it shows the borders between neighboring textures)
         vec2 uv = fract(uv_frag_in + 500);
-        color = vec4(uv.x, uv.y, 0.0F, 1.0F);
-        return;
+        return vec4(uv.x, uv.y, 0.0F, 1.0F);
     }
 
-    // Material properties
-    vec3 MaterialDiffuseColor = getSurfaceColor(model_position.y);
-    vec3 MaterialAmbientColor = vec3(0.1, 0.1, 0.1) * MaterialDiffuseColor;
-    vec3 MaterialSpecularColor = vec3(0.3, 0.3, 0.3);
+    return vec4(1.0F, 1.0F, 1.0F, 1.0F);
+}
 
-    vec3 lightDir = lightPosition - model_position;
+vec4 calcDirLight(DirLight light, Material material, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // combine results
+    vec3 lightColor = light.color * light.power;
+    vec3 ambient  = lightColor * material.ambient;
+    vec3 diffuse  = lightColor * diff * material.diffuse;
+    vec3 specular = lightColor * spec * material.specular;
+    vec3 result = ambient + diffuse + specular;
+    return vec4(result, 1.0F);
+}
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
+    return vec3(1.0F);
+    #if 0
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    // combine results
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+    #endif
+}
+
+vec4 calcLight3(PointLight light, Material material, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = light.position - model_position;
     float distance = length(lightDir);
 
     lightDir = normalize(lightDir);
@@ -120,29 +170,46 @@ void main() {
     //  - light is behind the triangle -> 0
     float cosTheta = clamp(dot(normal, lightDir), 0, 1);
 
-    // Eye vector (towards the camera)
-    vec3 cameraDir = normalize(cameraPosition - model_position);
     // Direction in which the triangle reflects the light
     vec3 reflectDir = reflect(-lightDir, normal);
     // Cosine of the angle between the Eye vector and the Reflect vector,
     // clamped to 0
     //  - Looking into the reflection -> 1
     //  - Looking elsewhere -> < 1
-    float cosAlpha = clamp(dot(cameraDir, reflectDir), 0, 1);
+    float cosAlpha = clamp(dot(viewDir, reflectDir), 0, 1);
 
-    // Ambient : simulates indirect lighting
-    vec3 colorv3 = MaterialAmbientColor;
+    vec3 color = material.ambient;
+    color += material.diffuse * lightColor * lightPower * cosTheta / (distance*distance);
+    color += material.specular * lightColor * lightPower * pow(cosAlpha, material.shininess) / (distance*distance);
+    return vec4(color, 1.0F);
+}
 
-    // Diffuse : "color" of the object
-    colorv3 += MaterialDiffuseColor * lightColor * lightPower * cosTheta / (distance*distance);
+void main() {
+    vec3 normal = normalize(normalMatrix * normal_frag_in);
+    vec3 tangent = normalize(normalMatrix * tangent_frag_in);
+    vec3 bitangent = normalize(normalMatrix * bitangent_frag_in);
+    vec3 viewDir = normalize(cameraPosition - model_position);
 
-    // Specular : reflective highlight, like a mirror
-    colorv3 += MaterialSpecularColor * lightColor * lightPower * pow(cosAlpha, 1024) / (distance*distance);
+    if (showNormals || showTangents || showUVs) {
+        color = debugColors(normal, tangent, bitangent);
+        return;
+    }
 
-    color = vec4(colorv3, 1.0F);
+    vec3 materialDiffuseColor = getSurfaceColor(model_position.y);
+    vec3 materialAmbientColor = vec3(0.1, 0.1, 0.1) * materialDiffuseColor;
+    vec3 materialSpecularColor = vec3(0.3, 0.3, 0.3);
+    Material material = Material(materialAmbientColor, materialDiffuseColor, materialSpecularColor, 8);
+    PointLight light = PointLight(lightPosition, lightColor, lightPower);
+    DirLight dirLight = DirLight(lightPosition, lightColor, lightPower);
 
+    color = vec4(0.0F);
+    color += calcLight3(light, material, normal, viewDir);
+//        color += calcDirLight(dirLight, material, normal, viewDir);
+}
+
+vec4 calcLight2() {
+    return vec4(1.0F, 1.0F, 1.0F, 1.0F);
     #if 0
-    #if 1
     vec3 materialAmbient = vec3(0.1F);
     vec3 materialDiffuse = getSurfaceColor(model_position.y);
     vec3 materialSpecular = vec3(0.1F);
@@ -158,7 +225,7 @@ void main() {
 
     // specular
     vec3 viewDir = normalize(cameraPosition - model_position);
-//    vec3 reflectDir = reflect(-lightDir, normal);
+    //    vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(dot(viewDir, reflectDir), materialShininess);
     vec3 specular = lightColor * (spec * materialSpecular);
 
@@ -166,8 +233,13 @@ void main() {
     result += ambient;
     result += diffuse;
     //    result += specular;
-    color = vec4(result, 1.0F);
-    #else
+    return vec4(result, 1.0F);
+    #endif
+}
+
+vec4 calcLight1() {
+    return vec4(1.0F, 1.0F, 1.0F, 1.0F);
+    #if 0
     float brightness = max(dot(normal, surfaceToLight), 0.0F);
     brightness *= lightPower;
     brightness = clamp(brightness, 0, 1);
@@ -181,14 +253,13 @@ void main() {
     float cosAlpha = max(dot(cameraDirection, reflectionDirection), 0.0F);
     specularColor *= lightColor * pow(cosAlpha, 2) * lightPower;
 
-    vec3 colorv3 = vec3(0.0);
+    vec3 color = vec3(0.0);
     vec3 ambientColor = vec3(0.1F);
-    colorv3 += ambientColor;
-    colorv3 += diffuseColor;
-    colorv3 += specularColor;
+    color += ambientColor;
+    color += diffuseColor;
+    color += specularColor;
 
-    color = vec4(colorv3, 1.0F);
-    #endif
+    return vec4(color, 1.0F);
     #endif
 }
 
