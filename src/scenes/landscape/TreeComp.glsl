bc580432fbe8cad7d161d430021c620f3aebe941
Hendrik Müller <henne90gen@gmail.com>
2021-02-20 18:26:38 +0100

    Integrated compute shader

--- src/scenes/landscape/TreeComp.glsl
+++ src/scenes/landscape/TreeComp.glsl
@@ -1,11 +1,74 @@
 #version 430 core
 
-layout (local_size_x = 1, local_size_y = 1) in;
-layout (rgba32f, binding = 0) uniform image2D img_output;
+#include "NoiseLib.glsl"
 
-uniform vec3 pixelColor = vec3(1.0F, 0.0F, 1.0F);
+layout (local_size_x = 1, local_size_y = 1) in;
+layout (rgba32f, binding = 0) uniform image2D imgOutput;
+
+uniform int gridWidth = 1000;
+
+uniform NoiseLayer noiseLayers[MAX_NUM_NOISE_LAYERS];
+uniform int numNoiseLayers;
+uniform float finiteDifference;
+uniform bool useFiniteDifferences;
+uniform float power;
+uniform float bowlStrength;
+uniform float platformHeight;
+uniform int seed;
+
+//void placementRandom(inout vec2 pos) {
+//    pos = vec2(gold_noise(pos.xx, float(gl_InstanceID)), gold_noise(pos.yy, float(gl_InstanceID)));
+//}
+
+vec2 rotate(vec2 v, float a) {
+    float s = sin(a);
+    float c = cos(a);
+    mat2 m = mat2(c, -s, s, c);
+    return m * v;
+}
+
+void placementJiggleAroundPos(inout vec2 pos) {
+    vec2 displacement = vec2(1.0F, 0.0);
+
+    float angle = snoise2(pos*100.0F).x;
+    displacement = rotate(displacement, angle);
+
+    float distance = snoise2(pos*100.0F).x / 10.0F;
+    displacement *= distance;
+
+    pos += displacement;
+}
+
+bool discardTree(vec2 pos, vec4 noise) {
+    float platformSize = 1000.0F / 15.0F;
+    if ((pos.x > -platformSize && pos.x < platformSize) && (pos.y > -platformSize && pos.y < platformSize)) {
+        return true;
+    }
+
+    vec3 tangent = vec3(1.0F, noise.y, 0.0F);
+    vec3 bitangent = vec3(0.0F, noise.z, 1.0F);
+    vec3 normal = -normalize(cross(tangent, bitangent));
+    float maxSlope = 0.7F;
+    if (normal.y > maxSlope) {
+        return true;
+    }
+
+    return false;
+}
 
 void main() {
-    ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
-    imageStore(img_output, pixel_coords, vec4(pixelColor, 1.0F));
+    // 0.0F - imageSize(imgOutput)
+    vec2 pos = vec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
+    // 0.0F - 1.0F
+    pos /= vec2(imageSize(imgOutput).x, imageSize(imgOutput).y);
+    // 0.0F - 1000.0F
+    pos *= gridWidth;
+    // -500.0F - 500.0F
+    pos -= gridWidth / 2.0F;
+
+    vec4 noise = generateHeight(pos, noiseLayers, numNoiseLayers, useFiniteDifferences, finiteDifference, power, bowlStrength, platformHeight, seed);
+    vec3 position = vec3(pos.x, noise.x, pos.y);
+
+    ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);
+    imageStore(imgOutput, pixelCoords, vec4(position, 1.0F));
 }
--- src/scenes/landscape/Trees.cpp
+++ src/scenes/landscape/Trees.cpp
@@ -44,6 +44,7 @@ void Trees::render(const glm::mat4 &projectionMatrix, const glm::mat4 &viewMatri
         // TODO(henne): compute shader execution can be moved into init
         compShader->bind();
         compShader->setUniform("pixelColor", glm::vec3(1.0F, 0.0F, 0.0F));
+        terrainParams.setShaderUniforms(compShader);
         GL_Call(glBindImageTexture(0, treePositionTextureId, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F));
         GL_Call(glDispatchCompute(treePositionTextureWidth, treePositionTextureHeight, 1));
         GL_Call(glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT));
@@ -98,10 +99,12 @@ void Trees::render(const glm::mat4 &projectionMatrix, const glm::mat4 &viewMatri
     shader->setUniform("modelMatrix", modelMatrix);
     shader->setUniform("viewMatrix", viewMatrix);
     shader->setUniform("projectionMatrix", projectionMatrix);
-
+    shader->setUniform("treeCount", treeCount);
+    shader->setUniform("positionTexture", 0);
     terrainParams.setShaderUniforms(shader);
 
-    shader->setUniform("treeCount", treeCount);
+    GL_Call(glActiveTexture(GL_TEXTURE0));
+    GL_Call(glBindTexture(GL_TEXTURE_2D, treePositionTextureId));
 
     if (shaderToggles.drawWireframe) {
         GL_Call(glPolygonMode(GL_FRONT_AND_BACK, GL_LINE));
@@ -131,22 +134,4 @@ void Trees::initComputeShaderStuff() {
     GL_Call(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, treePositionTextureWidth, treePositionTextureHeight, 0, GL_RGBA,
                          GL_FLOAT, nullptr));
     GL_Call(glBindImageTexture(0, treePositionTextureId, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F));
-
-    int work_grp_cnt[3];
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &work_grp_cnt[0]);
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &work_grp_cnt[1]);
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &work_grp_cnt[2]);
-    std::cout << "max global (total) work group counts x: " << work_grp_cnt[0] << " y: " << work_grp_cnt[1]
-              << " z: " << work_grp_cnt[2] << std::endl;
-
-    int work_grp_size[3];
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &work_grp_size[0]);
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &work_grp_size[1]);
-    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &work_grp_size[2]);
-    std::cout << "max local (in one shader) work group sizes x: " << work_grp_size[0] << " y: " << work_grp_size[1]
-              << " z: " << work_grp_size[2] << std::endl;
-
-    int work_grp_inv = 0;
-    glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &work_grp_inv);
-    std::cout << "max local work group invocations " << work_grp_inv << std::endl;
 }
