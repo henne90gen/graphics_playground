af615132042048a9c27f14462c851ceed312e0d5
Hendrik Müller <henne90gen@gmail.com>
2023-01-22 17:14:44 +0100

    Removed dependence on "click" library for build

--- scripts/copy_resources.py
+++ scripts/copy_resources.py
@@ -1,13 +1,14 @@
 import os
 import shutil
+import sys
 
 from typing import List
 
 
-def run_copy_resources(base_dir: str, files: List[str], dest: str):
+def run_copy_resources(base_dir: str, files: List[str], dest_dir: str):
     for f in files:
         src = f"{base_dir}/{f}"
-        dst = f"{dest}/{f}"
+        dst = f"{dest_dir}/{f}"
 
         if not os.path.exists(src):
             print(f"Could not find resource '{src}'")
@@ -23,3 +24,11 @@ def run_copy_resources(base_dir: str, files: List[str], dest: str):
             if os.path.exists(dst):
                 shutil.rmtree(dst)
             shutil.copytree(src, dst)
+
+
+def main():
+    run_copy_resources(sys.argv[1], [sys.argv[2]], sys.argv[3])
+
+
+if __name__ == "__main__":
+    main()
