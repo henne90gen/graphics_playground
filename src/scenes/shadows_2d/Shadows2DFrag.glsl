6e142a59827d2ede685d85e447961027a9ab8644
Hendrik Müller <hendrik.mueller@devboost.com>
2023-02-04 16:52:07 +0100

    Made shaders of Shadows2D compile with WebGL

--- src/scenes/shadows_2d/Shadows2DFrag.glsl
+++ src/scenes/shadows_2d/Shadows2DFrag.glsl
@@ -1,9 +1,10 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 uniform vec3 u_Color;
 
 out vec4 color;
 
 void main() {
-    color = vec4(u_Color, 1.0);
+    color = vec4(u_Color, 1.0F);
 }
