c4a932d2120b8f1353507f81116c221c2af03065
Hendrik Müller <hendrik.mueller@devboost.com>
2023-02-04 16:42:59 +0100

    Got ModelLoading to run on the web

--- src/scenes/model_loading/ModelLoadingFrag.glsl
+++ src/scenes/model_loading/ModelLoadingFrag.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 in vec3 v_Normal;
 in vec2 v_UV;
@@ -11,10 +12,10 @@ out vec4 color;
 
 void main() {
     if (u_HasNormals && !u_HasTexture) {
-        color = vec4(v_Normal, 1.0);
+        color = vec4(v_Normal, 1.0F);
     } else if (u_HasNormals && u_HasTexture) {
-        color = vec4(texture(u_TextureSampler, v_UV).rgb, 1.0);
+        color = vec4(texture(u_TextureSampler, v_UV).rgb, 1.0F);
     } else {
-        color = vec4(1.0, 1.0, 1.0, 1.0);
+        color = vec4(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
