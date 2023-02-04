4a5840dc7adc578226d6507968a9509081ee2519
Hendrik Müller <henne90gen@gmail.com>
2023-02-04 16:52:07 +0100

    Made shaders of Shadows2D compile with WebGL

--- src/scenes/shadows_2d/Shadows2DVert.glsl
+++ src/scenes/shadows_2d/Shadows2DVert.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 in vec2 a_Position;
 
@@ -7,5 +8,5 @@ uniform mat4 u_View;
 uniform mat4 u_Projection;
 
 void main() {
-    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 0.0, 1.0);
+    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 0.0F, 1.0F);
 }
