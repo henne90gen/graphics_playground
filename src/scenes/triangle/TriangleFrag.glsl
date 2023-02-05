7897899270317f71245ec72847862744e8a5b58c
Hendrik Müller <henne90gen@gmail.com>
2023-02-05 13:54:48 +0100

    Got Triangle to run on the web

--- src/scenes/triangle/TriangleFrag.glsl
+++ src/scenes/triangle/TriangleFrag.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 in vec3 passColor;
 in vec3 v_Position;
@@ -10,6 +11,6 @@ vec3 map(vec3 value, vec3 inMin, vec3 inMax, vec3 outMin, vec3 outMax) {
 }
 
 void main() {
-    vec3 finalColor = passColor * map(v_Position, vec3(-1), vec3(1), vec3(0), vec3(1));
-    color = vec4(finalColor, 1.0);
+    vec3 finalColor = passColor * map(v_Position, vec3(-1.0F), vec3(1.0F), vec3(0.0F), vec3(1.0F));
+    color = vec4(finalColor, 1.0F);
 }
--- src/scenes/triangle/TriangleVert.glsl
+++ src/scenes/triangle/TriangleVert.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 in vec2 position;
 in vec3 color;
@@ -8,6 +9,6 @@ out vec3 v_Position;
 
 void main() {
     passColor = color;
-    v_Position = vec3(position, 0);
-    gl_Position = vec4(v_Position, 1.0);
+    v_Position = vec3(position, 0.0F);
+    gl_Position = vec4(v_Position, 1.0F);
 }
