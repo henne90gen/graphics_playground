036ba3384f7e8c9c5a6d6992ef811a78827ce9dd
Hendrik Müller <henne90gen@gmail.com>
2023-02-07 23:01:40 +0100

    Added parameter for axis width

--- src/scenes/graphing_calculator/GraphingCalculator.cpp
+++ src/scenes/graphing_calculator/GraphingCalculator.cpp
@@ -19,16 +19,19 @@ void GraphingCalculator::destroy() {}
 void GraphingCalculator::tick() {
     static glm::vec2 boundsX = glm::vec2(0.0, 1.0);
     static glm::vec2 boundsY = glm::vec2(0.0, 1.0);
+    static float axisWidth = 0.001F;
 
     ImGui::Begin("Settings");
     ImGui::DragFloat2("Bounds X", reinterpret_cast<float *>(&boundsX), 0.1F);
     ImGui::DragFloat2("Bounds Y", reinterpret_cast<float *>(&boundsY), 0.1F);
+    ImGui::DragFloat("Axis Width", &axisWidth, 0.001F);
     ImGui::End();
 
     shader->bind();
 
     shader->setUniform("boundsX", boundsX);
     shader->setUniform("boundsY", boundsY);
+    shader->setUniform("axisWidth", axisWidth);
 
     va->bind();
     GL_Call(glDrawElements(GL_TRIANGLES, va->getIndexBuffer()->getCount(), GL_UNSIGNED_INT, nullptr));
