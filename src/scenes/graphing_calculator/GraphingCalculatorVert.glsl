8427fa1378c655965889ae4263fce40800a6fbef
Hendrik Müller <hendrik.mueller@devboost.com>
2023-04-21 20:25:45 +0200

    Improved function drawing

--- src/scenes/graphing_calculator/GraphingCalculatorVert.glsl
+++ src/scenes/graphing_calculator/GraphingCalculatorVert.glsl
@@ -4,9 +4,9 @@ precision mediump float;
 in vec3 a_Position;
 in vec2 a_UV;
 
-out vec2 coords;
+out vec2 positionScreenSpace;
 
 void main() {
     gl_Position = vec4(a_Position, 1.0);
-    coords = a_UV;
+    positionScreenSpace = a_UV;
 }
