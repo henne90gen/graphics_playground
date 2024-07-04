6083e146453541564eed964c918cb6e79e7ee417
Hendrik Müller <henne90gen@gmail.com>
2024-07-04 17:40:36 +0200

    Added new function to update texture with raw pointer

--- src/libs/opengl/gl/Texture.h
+++ src/libs/opengl/gl/Texture.h
@@ -24,6 +24,7 @@ class Texture {
                 unsigned int unpackAlignment = 4) const;
     void update(const std::vector<glm::vec3> &data, unsigned int width, unsigned int height,
                 unsigned int unpackAlignment = 4) const;
+    void update(const glm::vec3 *data, unsigned int width, unsigned int height, unsigned int unpackAlignment = 4) const;
     void update(const std::vector<glm::vec4> &data, unsigned int width, unsigned int height,
                 unsigned int unpackAlignment = 4) const;
 
